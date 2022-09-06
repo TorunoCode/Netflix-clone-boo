6a91bafc23bbc9d37ff5bbc7c3046293157fdc91
TorunoCode <thanhlam110230@gmail.com>
2022-09-06 15:16:19 +0700

    update

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -70,7 +70,7 @@ function Modal() {
                 <PlusIcon className="h-7 w-7"/>
               </button>
               <button className="modalButton">
-                <HandThumbUpIcon className="h-7 w-7 ml-2px -mt-5px"/>
+                <HandThumbUpIcon className="h-7 w-7 ml-0.5"/>
               </button>
             </div>
             <button className="modalButton" onClick={()=> setMuted(!muted)}>
@@ -81,7 +81,7 @@ function Modal() {
         <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
         <div className="space-y-6 text-lg">
           <div className="flex items-center space-x-2 text-sm">
-            <p className="font-semibold text-green-400">{movie!.vote_average*10}% Match</p>
+            <p className="font-semibold text-green-400">{Math.round(movie!.vote_average*1000)/100}% Match</p>
             <p className="font-light">{movie?.release_date || movie?.first_air_date}</p>
             <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
               HD
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import useAuth from '../hooks/useAuth'
 import { useRecoilValue } from 'recoil'
 import {modalState} from'../atoms/modalAtom'
 import Modal from '../components/Modal'
+import Plans from '../components/Plans'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -31,7 +32,9 @@ const Home = ({
   trendingNow,}:Props) => {
     const { loading} = useAuth()
     const showModal = useRecoilValue(modalState)
-    if(loading ) return null
+    const subscription = false
+    if(loading || subscription === null) return null
+    if(!subscription) return <Plans/>
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10
     to-[#010511] lg:h-[140vh]">
